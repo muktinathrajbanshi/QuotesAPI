08e78379674f922a74fa6dfb695c1cdaca9a8d00
unknown <muktinathrajbanshi786@gmail.com>
2026-01-26 15:37:00 +0545

    logic bulding part started re baba

---
+++ index.js
@@ -0,0 +1,11 @@
+const getQuotes = async () => {
+    const api = "https://zenquotes.io/api/quotes";
+    try {
+        let data = await fetch(api);
+    } catch (error) {
+        
+    }
+};
+
+getQuotes();
+

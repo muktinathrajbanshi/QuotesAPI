a10d3cd2e392ee56aacae2ed33c866b7c98378f3
unknown <muktinathrajbanshi786@gmail.com>
2026-01-26 16:06:18 +0545

    because of CORS i need to modify this logic re baba

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const getQuotes = async () => {
     const api = "https://zenquotes.io/api/quotes";
+    const proxy = "https://api.allorigins.win/get?url=" + encodeURIComponent(api);
+
     try {
-        let data = await fetch(api);
+        const response = await fetch(proxy);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        const dataWrapped = await response.json();
+        const data = JSON.parse(dataWrapped.contents); // actual quotes
+        console.log(data);
     } catch (error) {
-        
+        console.log("Error fetching quotes:", error);
     }
 };
 
 getQuotes();
-

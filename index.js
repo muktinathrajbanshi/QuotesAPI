1854616f5b97e09c7c9d114242709ee4667ba393
unknown <muktinathrajbanshi786@gmail.com>
2026-01-27 13:32:02 +0545

    for this API logic is implemented

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+const quotes = document.getElementById("quotes");
+let data = "";
+
+const getNewQuotes = () => {
+    let randomNum = Math.floor(Math.random() * 10);
+    quotes.innerText = `${data[randomNum].q}`
+}
 
 const getQuotes = async () => {
-    
     try {
         const response = await fetch("https://raw.githubusercontent.com/muktinathrajbanshi/quotes-api/refs/heads/main/index.json");
-        const data = await response.json();
-        console.log(data);
+         data = await response.json();
+        getNewQuotes();
+        // console.log(data[100].q);
+        // console.log(data[100].a);
+        
     } catch (error) {
         console.log(error);
     }
-
+ 
 };
 
 getQuotes();
